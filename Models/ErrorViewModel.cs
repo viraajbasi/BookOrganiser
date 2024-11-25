6821a4b3fb29ac2b7401c77cf464535c21e1af2c
Viraaj Basi <viraajbasi@gmail.com>
2024-11-25 14:54:12 +0000

    Created MVC Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BookOrganiser.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}

e77ba8a82749af719dd5f89c08a5a743b5a101aa
Viraaj Basi <viraajbasi@gmail.com>
2024-11-25 15:03:33 +0000

    Added Book Model

---
+++ Models/Book.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BookOrganiser.Models;
+
+public class Book
+{
+    [Key] public int ID { get; set; }
+    public string GoogleBooksID { get; set; } = string.Empty;
+    public string Title { get; set; } = string.Empty;
+    public string Subtitle { get; set; } = string.Empty;
+    public List<string> Authors { get; set; } = new List<string>();
+    public string Publisher { get; set; } = string.Empty;
+    public string PublishedDate { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string ISBN10 { get; set; } = string.Empty;
+    public string ISBN13 { get; set; } = string.Empty;
+    public int PageCount { get; set; }
+    public List<string> GoogleBooksCategories { get; set; } = new List<string>();
+    public string Thumbnail { get; set; } = string.Empty;
+    public string SmallThumbnail { get; set; } = string.Empty;
+    public string SmallImage { get; set; } = string.Empty;
+    public string MediumImage  { get; set; } = string.Empty;
+    public string LargeImage { get; set; } = string.Empty;
+    public string ExtraLargeImage { get; set; } = string.Empty;
+    public string GoogleBooksLink { get; set; } = string.Empty;
+    public string CustomCategories { get; set; } = string.Empty;
+}

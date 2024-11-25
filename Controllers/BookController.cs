49446185d1cbe0453fe518ab141d4beb230ed3ce
Viraaj Basi <viraajbasi@gmail.com>
2024-11-25 16:40:11 +0000

    Updated Controller

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -1,9 +1,16 @@
+using BookOrganiser.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookOrganiser.Controllers;
 
 public class BookController : Controller
 {
+    private readonly AppDbContext _context;
+    
+    public BookController(AppDbContext context)
+    {
+        _context = context;
+    }
     public IActionResult Details()
     {
         return View();

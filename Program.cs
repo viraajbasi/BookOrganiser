40143e4a938f6e11e2869569bc211d8e8470c00e
Viraaj Basi <viraajbasi@gmail.com>
2024-12-05 22:59:09 +0000

    changed index page

--- Program.cs
+++ Program.cs
@@ -36,7 +36,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Book}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 

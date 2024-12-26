5226610d041a8a83b1b06ac9329a826d962dab9f
Viraaj Basi <viraajbasi@gmail.com>
2024-12-26 15:39:00 +0000

    added migration and updated database

--- Migrations/20241226153832_InitUsers.Designer.cs
+++ Migrations/20241226153832_InitUsers.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BookOrganiser.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookOrganiser.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241226153832_InitUsers")]
+    partial class InitUsers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.0");

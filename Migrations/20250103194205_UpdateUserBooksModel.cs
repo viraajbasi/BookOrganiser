f9337971d28fa06819411f4d78c75cd4aa6bd487
Viraaj Basi <viraajbasi@gmail.com>
2025-01-03 19:46:44 +0000

    Changed type for ID in UserBooks model

---
+++ Migrations/20250103194205_UpdateUserBooksModel.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookOrganiser.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserBooksModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "UserBooks",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT")
+                .Annotation("Sqlite:Autoincrement", true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                table: "UserBooks",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+        }
+    }
+}
--- Models/UserBooks.cs
+++ Models/UserBooks.cs
@@ -2,7 +2,7 @@ namespace BookOrganiser.Models;
 
 public class UserBooks
 {
-    public string Id { get; set; } = string.Empty;
+    public int Id { get; set; }
     public string UserName { get; set; } = string.Empty;
-    public List<string> BookIds { get; set; } = new List<string>();
+    public List<string> BookIds { get; set; } = new();
 }

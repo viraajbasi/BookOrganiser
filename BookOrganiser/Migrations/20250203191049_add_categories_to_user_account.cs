6369d37ebcb8ce1ffee5da2d30edea15ce185d14
Viraaj Basi <viraajbasi@gmail.com>
2025-02-03 19:12:40 +0000

    Added categories to UserAccount model

---
+++ BookOrganiser/Migrations/20250203191049_add_categories_to_user_account.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookOrganiser.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_categories_to_user_account : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserCategories",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "[]");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserCategories",
+                table: "AspNetUsers");
+        }
+    }
+}

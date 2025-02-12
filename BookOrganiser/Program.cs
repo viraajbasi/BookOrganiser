bfb261b4521fb419f85ab30a3662e74172c84ea0
Viraaj Basi <viraajbasi@gmail.com>
2025-02-12 15:44:04 +0000

    added basic error handling for http status codes

--- BookOrganiser/Program.cs
+++ BookOrganiser/Program.cs
@@ -34,6 +34,8 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+app.UseStatusCodePagesWithRedirects("/Home/Error?statusCode={0}");
+
 app.UseHttpsRedirection();
 app.UseRouting();
 

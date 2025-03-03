a6f39534bcab993a3490086c0b6b677ccd33406a
Viraaj Basi <viraajbasi@gmail.com>
2025-03-03 18:11:03 +0000

    updated User Account model

---
+++ BookOrganiser/Migrations/20250303180833_add_ai_toggle_to_user_account.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookOrganiser.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_ai_toggle_to_user_account : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "AcceptedAIFeatures",
+                table: "AspNetUsers",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AcceptedAIFeatures",
+                table: "AspNetUsers");
+        }
+    }
+}

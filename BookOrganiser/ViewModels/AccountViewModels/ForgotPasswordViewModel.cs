e7d1b1f99e60dc88e90db4bd3861cc5fd57812ce
Viraaj Basi <viraajbasi@gmail.com>
2025-04-13 05:10:16 +0100

    updated error message

--- BookOrganiser/ViewModels/AccountViewModels/ForgotPasswordViewModel.cs
+++ BookOrganiser/ViewModels/AccountViewModels/ForgotPasswordViewModel.cs
@@ -9,7 +9,7 @@ public class ForgotPasswordViewModel
     public string Email { get; set; } = string.Empty;
 
     [Required(ErrorMessage = "A password is required.")]
-    [StringLength(40, MinimumLength = 8, ErrorMessage = "The {0} must be at least {2} and at most {0} characters long.")]
+    [StringLength(40, MinimumLength = 8, ErrorMessage = "The password must be at least 8 characters long.")]
     [DataType(DataType.Password)]
     [Compare("ConfirmNewPassword", ErrorMessage = "Passwords don't match.")]
     [Display(Name = "New Password")]
